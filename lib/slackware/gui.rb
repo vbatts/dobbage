e21efa955d057ecb1925d5fad7bce82ea2af3aff
Vincent Batts <vbatts@hashbangbash.com>
2011-06-01 23:03:09 -0400

    adding a flag parser, and version flags

--- lib/slackware/gui.rb
+++ lib/slackware/gui.rb
@@ -1,24 +1,52 @@
-#!/usr/bin/env ruby
 # 2010, 2011 Vincent Batts, vbatts@hashbangbash.com
 #
 # Main file of Slackware package viewer.
 #
+# A good reference, is the qtsamuri example_02.rb
 
 # Load our QtRuby main window class:
 require 'slackware/gui/dobbage_window'
+require 'optparse'
 
-# Another good example, is the qtsamuri example_02.rb
+module Slackware::Gui
+	class Args
+		def self.parse(*args)
+			options = {}
 
-module Slackware
-	module Gui
-		# Launch the application:
-		def self::launch(*args)
-		  args.flatten!.compact!
-		  app = Qt::Application.new(args)
-		  window = DobbageWindow.new(args)
-		  window.show
-		  app.exec
+			opts = OptionParser.new(args) {|opts|
+				opts.banner = "Usage: dobbage [options]"
+				opts.separator("")
+				opts.separator("Optional Flags:")
+
+				opts.on_tail("-v","--version","show version information") {|o|
+					puts <<-EOF
+Slackware Linux Version: #{SLACKWARE_VERSION}
+slack-utils version: #{UTILS_VERSION}
+dobbage version: #{DOBBAGE_VERSION}
+EOF
+					exit
+				}
+				opts.on_tail("-h","--help","show this help message") {|o|
+					puts opts
+					exit
+				}
+			}
+
+			opts.parse!
+			
+			return options
 		end
+	end
+
+	# Launch the application:
+	def self::launch(*args)
+	  args.flatten!.compact!
+	  options = Slackware::Gui::Args.parse(args)
+
+	  app = Qt::Application.new(args)
+	  window = DobbageWindow.new(args)
+	  window.show
+	  app.exec
 	end
 end
 
